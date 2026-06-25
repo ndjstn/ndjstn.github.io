67f2e9dcaa3dd6a114fe780b2369dbd5f08bb48a
Justin Stone <ndjstn@gmail.com>
2026-06-25 03:34:10 -0500

    Deploy portfolio without custom domain redirect

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,41 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1782376449',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+        '/projects/',
+      
+        '/contact/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+
+      
+      
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js',
+        '/assets/js/dist/theme.min.js'
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
